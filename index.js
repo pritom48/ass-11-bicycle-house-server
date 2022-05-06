6ac8a9e6e1c425d81579de47fc2cbe17b0dd1410
pritom48 <pritamdas41148@gmail.com>
2022-05-06 10:53:01 +0600

    server is runing

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 //MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('server in runing like cycle!!')
+})
+
+app.listen(port, () => {
+    console.log('curd in runing')
+})
